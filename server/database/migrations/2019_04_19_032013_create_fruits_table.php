40026324aaca3a5c95f0f4769f0186e89c54a2fa
sogaoh <sogaoh@ant-in-giant.work>
2019-04-19 12:44:51 +0900

    add Sample Table & Controller skelton

---
+++ server/database/migrations/2019_04_19_032013_create_fruits_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFruitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fruits', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->comment('フルーツ名');
+            $table->integer('price')->comment('フルーツ価格');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fruits');
+    }
+}
